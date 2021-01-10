6ece224984f59610092aa58196aa4d3df2aaf13a
Sergei Koshel <unicgm@gmail.com>
2021-01-10 15:15:29 +0100

    Create a public S3 bucket with static content

--- modules/aws-s3-static-website-bucket/main.tf
+++ modules/aws-s3-static-website-bucket/main.tf
@@ -1,6 +1,7 @@
 # Terraform configuration
 
-resource "aws_s3_bucket" "s3_bucket" {
+# Create a bucket
+resource "aws_s3_bucket" "s3_app_bucket" {
   bucket = var.bucket_name
 
   acl    = "public-read"
@@ -29,4 +30,23 @@ EOF
   }
 
   tags = var.tags
+}
+
+# `template_files` module among other things calculates content type.
+# Thus, it's convenient to use it to list files for uploading to S3
+module "template_files" {
+  source = "hashicorp/dir/template"
+  version = "1.0.2"
+  base_dir = "${path.module}/www"
+}
+
+# Upload content of the www folder
+resource "aws_s3_bucket_object" "www_content" {
+  for_each = module.template_files.files
+  bucket = aws_s3_bucket.s3_app_bucket.id
+  key = each.key
+  content_type = each.value.content_type
+  source  = each.value.source_path
+  content = each.value.content
+  etag = each.value.digests.md5
 }
--- modules/aws-s3-static-website-bucket/outputs.tf
+++ modules/aws-s3-static-website-bucket/outputs.tf
@@ -2,15 +2,15 @@
 
 output "arn" {
   description = "ARN of the bucket"
-  value       = aws_s3_bucket.s3_bucket.arn
+  value       = aws_s3_bucket.s3_app_bucket.arn
 }
 
 output "name" {
   description = "Name (id) of the bucket"
-  value       = aws_s3_bucket.s3_bucket.id
+  value       = aws_s3_bucket.s3_app_bucket.id
 }
 
 output "website_endpoint" {
   description = "Domain name of the bucket"
-  value       = aws_s3_bucket.s3_bucket.website_endpoint
+  value       = aws_s3_bucket.s3_app_bucket.website_endpoint
 }
